75e5c8db01c160e44bd3ef241cec084449a5855e
page-cloud <79392258+page-cloud@users.noreply.github.com>
2023-03-25 14:59:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From page cloud" }');
+    res.send('{ "Message": "Hello From page cloud Academy" }');
 });
 
 app.get('/will', function (req, res) {
